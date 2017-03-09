2d4bd2370d365c71711f9e46cd75dd4ed589b882
Micah Stubbs <micah.stubbs@gmail.com>
2017-03-08 18:16:52 -0800

    add iteration 05 with the firm.csv static dataset

---
+++ 05/README.md
@@ -0,0 +1,7 @@
+now with links! graph from [Networks - Graphs 5](https://bl.ocks.org/emeeks/39fea1d900964379416b)
+
+a force layout where like-nodes cluster together, drawn from static data
+
+an iteration on the block [Clustered Force Layout 4.0](https://bl.ocks.org/shancarter/f621ac5d93498aa1223d8d20e5d3a0f4) from [@shancarter](https://twitter.com/shancarter)
+
+while [es-linting](http://eslint.org/docs/rules/no-mixed-operators) code from this block, I read up on [operator precendence](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence) in Javascript. It turns out that multiplication `*` is evaluated before division `/`.
---
+++ 05/vis.js
@@ -0,0 +1,134 @@
+/* global d3 */
+
+d3.json('graph.json', (error, graph) => {
+  console.log('graph', graph);
+  const nodes = graph.nodes;
+  const links = graph.links;
+
+  const margin = { top: 100, right: 100, bottom: 100, left: 100 };
+
+  const width = 960;
+  const height = 500;
+
+  // separation between same-color circles
+  const padding = 1.5;
+
+  // separation between different-color circles
+  const clusterPadding = 6;
+
+  const maxRadius = 12;
+
+  const z = d3.scaleOrdinal(d3.schemeCategory20);
+
+  // total number of nodes
+  const n = nodes.length;
+
+  // detect communities with jsLouvain
+  var nodeData = nodes.map(function (d) {return d.id});
+  var linkData = links.map(function (d) {return {source: d.source.id, target: d.target.id, weight: 1}; });
+
+  var community = jLouvain()
+    .nodes(nodeData)
+    .edges(linkData);
+
+  var result  = community();
+
+  const defaultRadius = 10;
+  nodes.forEach(function (node) {
+    node.r = defaultRadius;
+    node.id = Number(node.id);
+    node.cluster = result[node.id]
+  });
+
+  // collect clusters from nodes
+  const clusters = {};
+  nodes.forEach((node) => {
+    const radius = node.r;
+    const clusterID = node.cluster;
+    if (!clusters[clusterID] || (radius > clusters[clusterID].r)) { 
+      clusters[clusterID] = node;
+    }
+  });
+  console.log('clusters', clusters);
+
+  const svg = d3.select('body')
+    .append('svg')
+    .attr('height', height)
+    .attr('width', width)
+    .append('g')
+      .attr('transform', `translate(${width / 2},${height / 2})`);
+
+  const circles = svg.append('g')
+    .datum(nodes)
+    .selectAll('.circle')
+    .data(d => d)
+    .enter().append('circle')
+      .attr('r', d => d.r)
+      .attr('fill', d => z(d.cluster))
+      .attr('stroke', 'black')
+      .attr('stroke-width', 1);
+
+  const simulation = d3.forceSimulation(nodes)
+    .velocityDecay(0.2)
+    .force('x', d3.forceX().strength(0.0005))
+    .force('y', d3.forceY().strength(0.0005))
+    .force('collide', collide)
+    .force('cluster', clustering)
+    .on('tick', ticked);
+
+  function ticked() {
+    circles
+      .attr('cx', d => d.x)
+      .attr('cy', d => d.y);
+  }
+
+  // These are implementations of the custom forces
+  function clustering(alpha) {
+    nodes.forEach((d) => {
+      const cluster = clusters[d.cluster];
+      if (cluster === d) return;
+      let x = d.x - cluster.x;
+      let y = d.y - cluster.y;
+      let l = Math.sqrt((x * x) + (y * y));
+      const r = d.r + cluster.r;
+      if (l !== r) {
+        l = ((l - r) / l) * alpha;
+        d.x -= x *= l;
+        d.y -= y *= l;
+        cluster.x += x;
+        cluster.y += y;
+      }
+    });
+  }
+
+  function collide(alpha) {
+    const quadtree = d3.quadtree()
+      .x(d => d.x)
+      .y(d => d.y)
+      .addAll(nodes);
+
+    nodes.forEach((d) => {
+      const r = d.r + maxRadius + Math.max(padding, clusterPadding);
+      const nx1 = d.x - r;
+      const nx2 = d.x + r;
+      const ny1 = d.y - r;
+      const ny2 = d.y + r;
+      quadtree.visit((quad, x1, y1, x2, y2) => {
+        if (quad.data && (quad.data !== d)) {
+          let x = d.x - quad.data.x;
+          let y = d.y - quad.data.y;
+          let l = Math.sqrt((x * x) + (y * y));
+          const r = d.r + quad.data.r + (d.cluster === quad.data.cluster ? padding : clusterPadding);
+          if (l < r) {
+            l = ((l - r) / l) * alpha;
+            d.x -= x *= l;
+            d.y -= y *= l;
+            quad.data.x += x;
+            quad.data.y += y;
+          }
+        }
+        return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+      });
+    });
+  }
+});
